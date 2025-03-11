ffe68c6e1584b587437307a52434ba4c0b4af4c3
javanfredrick <javantabale@gmail.com>
2025-03-11 07:46:33 +0300

    first commit

--- calculator2.py
+++ calculator2.py
@@ -4,7 +4,7 @@ def calculator():
     try:
         num1 = float(input("Enter the first number: "))
         num2 = float(input("Enter the second number: "))
-        operation = input("Enter the operation (+, -, *, /): ")
+        operation = input("Enter the operation (+, -, *, /)")
 
         if operation == "+":
             result = num1 + num2
